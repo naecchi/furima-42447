5cb999df26f49491795993db91cc7041a51f354d
naecchi <71973149+naecchi@users.noreply.github.com>
2025-05-29 16:03:46 +0900

    first commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,26 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link application.css
+
+//= link_base/reset.css
+
+//= link_items/exhibit-btn.css
+//= link_items/index.css
+//= link_items/new.css
+//= link_items/show.css
+
+
+//= link_orders/index.css
+
+//= link_shared/error_messages.css
+//= link_shared/footer.css
+//= link_shared/header.css
+//= link_shared/second_fotter.css
+//= link_shared/second_header.css
+
+//= link_user/login.css
+//= link_user/registration.css
+
+
